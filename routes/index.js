e8bebc395f811cf11c274dbc1847a07a8fb95765
steavn-Chen-g <tom9876555@gmail.com>
2022-04-14 00:12:58 +0800

    feat: add user modules and add login page

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
 const sort = require('./modules/sort')
+const users = require('./modules/users.js')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/search', search)
 router.use('/sort', sort)
+router.use('/users', users)
 
 module.exports = router;
--- routes/modules/search.js
+++ routes/modules/search.js
@@ -4,27 +4,41 @@ const router = express.Router();
 const Restaurant = require("../../models/restaurant");
 
 router.get("/", (req, res) => {
-  const keyword = req.query.keyword;
+  const keyword = req.query.keyword.trim()
   let errorMessage;
   if (!keyword) {
     errorMessage = "請輸入想要搜尋的字元 !";
     return res.render("errorSearch", { message: errorMessage });
   }
-  return Restaurant.find()
-    .lean()
-    .then((restaurants) => {
-      const results = restaurants.filter((i) => {
-        return (
-          i.name.toLowerCase().includes(keyword.toLowerCase()) ||
-          i.category.toLowerCase().includes(keyword.toLowerCase())
-        );
-      });
-      if (results.length === 0) {
-        errorMessage = "沒有找到輸入相關字元的餐廳 !";
-        return res.render("errorSearch", { message: errorMessage });
-      }
-      res.render("index", { restaurants: results });
-    });
+  // 第一種寫法
+  return Restaurant.find({$or:[ { name: { $regex: keyword, $options: 'i' } }, { category: { $regex: keyword, $options: 'i' } } ]})
+  .lean()
+  .then(restaurants => {
+    if (restaurants.length === 0) {
+      errorMessage = "沒有找到相關字元的餐廳 !";
+      return res.render("errorSearch", { message: errorMessage });
+    }
+
+    res.render('index', { restaurants: restaurants})
+  })
+  .catch(err => console.error(err))
+
+  // 第二種寫法
+  // return Restaurant.find()
+  //   .lean()
+  //   .then((restaurants) => {
+  //     const results = restaurants.filter((i) => {
+  //       return (
+  //         i.name.toLowerCase().includes(keyword.toLowerCase()) ||
+  //         i.category.toLowerCase().includes(keyword.toLowerCase())
+  //       );
+  //     });
+  //     if (results.length === 0) {
+  //       errorMessage = "沒有找到輸入相關字元的餐廳 !";
+  //       return res.render("errorSearch", { message: errorMessage });
+  //     }
+  //     res.render("index", { restaurants: results });
+  //   });
 });
 
 module.exports = router;
